c66cde06bd824835a4074774e967316200180f1e
shafeer553 <shafi.shafeer@gmail.com>
2019-05-07 17:51:52 -0400

    jenkins code added

--- hello.java
+++ hello.java
@@ -2,4 +2,7 @@ public classs Hello
 {
 public static void main(string[] args)
 system.out.println("hello")
+system.out.println("GitHub hello")
+system.out.println("Jenkins hello")
+system.out.println("Jenkins hello")
 }
